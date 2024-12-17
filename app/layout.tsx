8cf428fb10238e4407b49763fa061e00d9000844
Mayank Kashyap <mayankkashyap@Mayanks-MacBook-Pro.local>
2024-12-17 16:43:36 +0530

    Minor UI change

--- app/layout.tsx
+++ app/layout.tsx
@@ -273,13 +273,13 @@ export default function RootLayout({
                     ))}
                   </div>
                 </details>
-                <Link
+                {/* <Link
                   href="/contact"
                   onClick={() => setMobileMenuOpen(false)}
                   className="block py-2 text-gray-700 dark:text-gray-200 hover:text-blue-500 dark:hover:text-blue-400"
                 >
                   Contact
-                </Link>
+                </Link> */}
                 <Link
                   href="/about"
                   onClick={() => setMobileMenuOpen(false)}
